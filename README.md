55a1ec9d06d1a256d695a6eb2bb6af23e61ef6ef
Sachin Patel <sppatel@us.ibm.com>
2015-08-12 07:18:35 -0400

    Add initial extension files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # taurus_jmeter
-A pipeline test extesnion for Taurus/JMeter
+**(Experimental)** A pipeline test extension for Taurus/JMeter
+
+# Documentation
+
+# References
+* [Taurus](http://gettaurus.org/docs/Index/)
+* [Taurus-Github](https://github.com/Blazemeter/taurus)
+* [Blazemeter](https://blazemeter.com)
+* [JMeter](http://jmeter.apache.org)
---
+++ run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys
+import logging
+import os
+
+DEBUG = os.environ.get('DEBUG')
+
+# ascii color codes for output
+LABEL_GREEN = '\033[0;32m'
+LABEL_RED = '\033[0;31m'
+LABEL_COLOR = '\033[0;33m'
+LABEL_NO_COLOR = '\033[0m'
+STARS = "**********************************************************************"
+
+def setup_logging():
+    logger = logging.getLogger('pipeline')
+    if DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # if logmet is enabled, send the log through syslog as well
+    if os.environ.get('LOGMET_LOGGING_ENABLED'):
+        handler = logging.handlers.SysLogHandler(address='/dev/log')
+        logger.addHandler(handler)
+        # don't send debug info through syslog
+        handler.setLevel(logging.INFO)
+
+    # in any case, dump logging to the screen
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    if DEBUG:
+        handler.setLevel(logging.DEBUG)
+    else:
+        handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
+
+# Start
+LOGGER = setup_logging()
